da9aa9f0a292d894bba1ccd7f4ea9a0058e768ed
Долгов Никита <nik.dolgov.05@mail.ru>
2023-12-19 19:26:56 +0300

    Доработал 404 ошибку

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,24 @@
-import Link from "next/link";
-import Image from "next/image";
-import React from "react";
+import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react';
 import style from './page.module.scss';
 export default function NotFound() {
   return (
     <div>
-      <Image alt="404" src="/404.png" width={600} height={600} className={style.error} />
-      <h1 className={style.txt}>404</h1>
-      {/* <div>
-        <Link href="/">Go back to Home</Link>
-      </div> */}
+      <section className={style.notFound}>
+        <div className={style.notFound__container}>
+          <Link href={'/'} className={style.notFound__inner}>
+            <Image
+              alt="404"
+              src="/404.png"
+              width={600}
+              height={600}
+              className={style.notFound__image}
+            />
+            <h1 className={style.notFound__title}>Not Found:<br/>Error 404</h1>
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }
