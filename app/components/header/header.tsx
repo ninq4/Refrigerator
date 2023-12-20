cfcbf34c4b8df8fdcb3b6b9b14eb17fdf675f60b
Долгов Никита <nik.dolgov.05@mail.ru>
2023-12-20 13:26:51 +0300

    Адаптировал шапку

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -9,11 +9,15 @@ import user from '@/public/user.svg';
 
 import { DomenButton } from './domensButton/domensButton';
 import { useOpenVidget } from '../../hooks/useOpenVidget';
+import { useOpenBurger } from '../../hooks/useOpenBurger';
 import { HeaderTopMenuItem } from './HeaderMenuItem/headerMenuItem';
 import { HeaderBottomMenuItem } from './HeaderBottomMenuItem/headerBottomMenuItem';
+import { BurgerButton } from './burgerButton/burgerButton';
 
 export default function Header() {
   const { open, handleToggle } = useOpenVidget(false);
+  const { openBurger, handleToggleBurger} = useOpenBurger(false);
+
   const menuItems = [
     { link: '/', image: Card, alt: 'Card' },
     { link: '/', image: BookMark, alt: 'BookMark' },
@@ -24,39 +28,75 @@ export default function Header() {
 
   return (
     <header className={`body__header ${style.header}`}>
-      <div className={style.header__container}>
-        <div className={style.header__inner}>
-          <div className={style.header__top}>
-            <Link href={"/"} className={`${style.header__top_logo} ${open ? `${style.active}` : ''}`}>
-              Fucking. Refrigerator
-            </Link>
-            <menu className={style.header__top_menu}>
-              {menuItems.map((item, pos) => (
-                <HeaderTopMenuItem
-                  key={pos}
-                  link={item.link}
-                  image={item.image}
-                  alt={item.alt}
-                  text={item.text}
-                  open={open}
-                />
-              ))}
+      <div className={style.header__block}>
+        <div className={style.header__container}>
+          <div className={style.header__inner}>
+            <div className={style.header__top}>
+              <div className={style.header__top_wrapper}>
+                <BurgerButton openBurger={openBurger} handleToggleBurger={handleToggleBurger} />
+                <Link
+                  href={'/'}
+                  className={`${style.header__top_logo} ${open ? `${style.active}` : ''} ${
+                    openBurger ? `${style.active_burger}` : ''
+                  }`}>
+                  Fucking. Refrigerator
+                </Link>
+              </div>
+              <menu className={style.header__top_menu}>
+                {menuItems.map((item, pos) => (
+                  <HeaderTopMenuItem
+                    key={pos}
+                    link={item.link}
+                    image={item.image}
+                    alt={item.alt}
+                    text={item.text}
+                    open={open}
+                  />
+                ))}
+              </menu>
+            </div>
+            <menu
+              className={`${style.header__bottom_menu} ${
+                openBurger ? `${style.active_burger}` : ''
+              }`}>
+              <HeaderBottomMenuItem
+                link={'/hostings'}
+                text="Хостинг"
+                alt="Hosting"
+                open={open}
+                handleToggleBurger={handleToggleBurger}
+              />
+              <HeaderBottomMenuItem
+                link={'/VDSVPS'}
+                text="VDS и VPS"
+                alt="VDS and VPS"
+                open={open}
+                openBurger={openBurger}
+                handleToggleBurger={handleToggleBurger}
+              />
+              <HeaderBottomMenuItem link={'/mail'} text="Почта" alt="Mail" open={open} />
+              <DomenButton open={open} handleToggleBurger={handleToggleBurger} openBurger={openBurger} handleToggle={handleToggle} />
+              <HeaderBottomMenuItem
+                link="/servers"
+                text="Выделенные сервера"
+                alt="Selected servers"
+                handleToggleBurger={handleToggleBurger}
+              />
+              <HeaderBottomMenuItem
+                link={'/constructor'}
+                text="Конструктор сайтов"
+                alt="Website builder"
+                handleToggleBurger={handleToggleBurger}
+              />
+              <HeaderBottomMenuItem
+                link="/"
+                text="Партнерская программа"
+                alt="Partnership program"
+                open={open}
+                handleToggleBurger={handleToggleBurger}
+              />
             </menu>
           </div>
-          <menu className={style.header__bottom_menu}>
-            <HeaderBottomMenuItem link={'/hostings'} text="Хостинг" alt="Hosting" open={open} />
-            <HeaderBottomMenuItem link={"/VDSVPS"} text="VDS и VPS" alt="VDS and VPS" open={open} />
-            <HeaderBottomMenuItem link={"/mail"} text="Почта" alt="Mail" open={open} />
-            <DomenButton open={open} handleToggle={handleToggle} />
-            <HeaderBottomMenuItem link={"/dedicated-servers"} text="Выделенные сервера" alt="Selected servers" />
-            <HeaderBottomMenuItem link="/WebsiteBuilder" text="Конструктор сайтов" alt="Website builder" />
-            <HeaderBottomMenuItem
-              link={"/AffiliateProgram"}
-              text="Партнерская программа"
-              alt="Partnership program"
-              open={open}
-            />
-          </menu>
         </div>
       </div>
     </header>
